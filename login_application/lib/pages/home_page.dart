9c288c12fb0cc0c6497fcd1d7513a20e987048ad
NERODEMON25 <nerodemon.ap@gmail.com>
2021-09-28 18:14:11 +0530

    Basic login ui

---
+++ login_application/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String name = 'nero';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("wellcome to   flutter  page by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
